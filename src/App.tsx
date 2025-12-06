280c6c189009ef07add16c13c1c568a033916900
Hadise Moradi <h4disem0radi@gmail.com>
2025-12-06 12:13:58 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+//import { Toaster } from "@/components/ui/toaster";
+// { Toaster as Sonner } from "@/components/ui/sonner";
+//import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageContext";
@@ -14,9 +14,9 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <LanguageProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
+          {/* <TooltipProvider> */}
+        {/* <Toaster /> */}
+         {/* <Sonner /> */}
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -25,7 +25,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </TooltipProvider>
+       {/* </TooltipProvider> */}
     </LanguageProvider>
   </QueryClientProvider>
 );
