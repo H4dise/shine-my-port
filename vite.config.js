8eaf5d72790c794578081c8c97f4653b1ab44fc7
Hadise Moradi <h4disem0radi@gmail.com>
2025-12-06 13:44:36 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/shine-my-port",
+  base: "/",  // 🔴 اینجا "/" باشه نه "/shine-my-port/"
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 })
+
